5667a6e5fea5522b93bf5fdfce9c29023a121611
gnsunayana <gnsunayana12@gmail.com>
2022-03-02 16:40:09 +0100

    Java based configuration changes for Primary and Profile annotations.

--- spring-di/src/main/java/com/springframework/springdi/config/GreetingServiceConfig.java
+++ spring-di/src/main/java/com/springframework/springdi/config/GreetingServiceConfig.java
@@ -1,10 +1,10 @@
 package com.springframework.springdi.config;
 
-import com.springframework.springdi.services.ConstructorGreetingService;
-import com.springframework.springdi.services.PropertyInjectedGreetingService;
-import com.springframework.springdi.services.SetterInjectedGreetingService;
+import com.springframework.springdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -22,5 +22,25 @@ public class GreetingServiceConfig {
     @Bean
     SetterInjectedGreetingService setterInjectedGreetingService(){
         return new SetterInjectedGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+          return new PrimaryGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(){
+        return new I18nEnglishGreetingService();
+    }
+    // If you dont give the name in the bean annotation, the name of the bean created will be the method name by default. You can
+    // override it by giving a name in the @Bean annotation. In his example he changed the value of the method name for one
+    // method and overrided the other using bean annotation.
+    @Profile({"ES","default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
     }
 }
--- spring-di/src/main/java/com/springframework/springdi/services/I18nEnglishGreetingService.java
+++ spring-di/src/main/java/com/springframework/springdi/services/I18nEnglishGreetingService.java
@@ -2,9 +2,9 @@ package com.springframework.springdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
+/*
 @Profile("EN")
-@Service("i18nService")
+@Service("i18nService")*/
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
